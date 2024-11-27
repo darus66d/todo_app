47d07973994c34a35f4bd8c3829d8073ba253c48
Darus Salam <darussalam66d@gmail.com>
2024-11-27 23:31:51 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -17,9 +17,20 @@ class ToDoApp extends StatelessWidget {
 }
 
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+
   const HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  List<todo> todos=[];
+  final titleTEController = TextEditingController();
+
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +38,52 @@ class HomeScreen extends StatelessWidget {
         title: Center(child: Text("ToDo App")),
         backgroundColor: Colors.deepPurple.shade200,
       ),
+      body: ListView.builder(
+          itemCount: todos.length,
+          itemBuilder: (context,index){
+        return ListTile(
+          title: Text(todos[index].title),
+          trailing: IconButton(onPressed: (){
+
+            todos.removeAt(index);
+            setState(() {
+
+            });
+
+          }, icon:Icon(Icons.delete)),
+        );
+      }),
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        showModalBottomSheet(context: context, builder: (context){
+          return Container(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                 children: [
+                   TextField(
+                     controller: titleTEController,
+                   ),
+                   ElevatedButton(onPressed: (){
+                     todos.add(todo(titleTEController.text, false));
+                     setState(() {
+
+                     });
+                   }, child: Text("Add")),
+                 ],
+              ),
+            ),
+
+
+
+          );
+        });
+      },child: Icon(Icons.add),),
     );
   }
 }
+class todo {
+  String title;
+  bool isdone;
 
+  todo(this.title, this.isdone);
+}
